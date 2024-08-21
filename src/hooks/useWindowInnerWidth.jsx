235c9bc77f2d73e226f53f46ebb936b68c43419d
Hussein Kandil <101815486+hussein-m-kandil@users.noreply.github.com>
2024-08-21 10:35:19 +0300

    Document useWindowInnerWidth custom React Hook

--- src/hooks/useWindowInnerWidth.jsx
+++ src/hooks/useWindowInnerWidth.jsx
@@ -1,5 +1,18 @@
 import { useState, useEffect } from "react";
 
+/**
+ * A custom React Hook that tracks the window's inner width,
+ * causing a re-render on every width change,
+ * and returns the updated width value.
+ *
+ * This Hook is built using the useState and useEffect Hooks.
+ *
+ * It provides a clean useEffect callback
+ * that removes the event listener from the window object
+ * before the component unmounts, ensuring better performance.
+ *
+ * @returns {number} - The current window's inner width
+ */
 export function useWindowInnerWidth() {
   const [width, setWidth] = useState(window.innerWidth);
 
