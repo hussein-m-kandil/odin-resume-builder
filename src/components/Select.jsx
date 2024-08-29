e3acd1d9700618ee1e374d62e2012076d19a016e
Hussein Kandil <101815486+hussein-m-kandil@users.noreply.github.com>
2024-08-29 15:42:15 +0300

    Truncate select options if not viewed on screen
    Render the full list of options only if the Select component is
    on screen, Otherwise, render small number of options to avoid
    rendering useless long list options and maintain better performance

--- src/components/Select.jsx
+++ src/components/Select.jsx
@@ -1,8 +1,41 @@
+import { useState, useRef, useEffect } from "react";
+
 function Select({ id, title, labelStyle, inputStyle, options, ...props }) {
+  const [onScreen, setOnScreen] = useState(false);
+  const thisRef = useRef(null);
+
+  // Add the full list of options only when the component on screen
+  // to avoid rendering useless long list of options
+  // and to maintain better performance
+  useEffect(() => {
+    if (IntersectionObserver && thisRef.current) {
+      const handleIntersection = (entries) => {
+        if (entries[0]?.isIntersecting) {
+          setOnScreen(true);
+        } else {
+          setOnScreen(false);
+        }
+      };
+      const observer = new IntersectionObserver(handleIntersection);
+      observer.observe(thisRef.current);
+      return () => observer.disconnect();
+    }
+  }, []);
+
+  const customOptions = onScreen ? options : options.slice(0, 15);
+
   return (
-    <label htmlFor={id} title={title} style={labelStyle}>
-      <select id={id} style={inputStyle} {...props}>
-        {options.map((opt, i) => (
+    <label ref={thisRef} htmlFor={id} title={title} style={labelStyle}>
+      <select
+        id={id}
+        style={{
+          // Only for font size select menu
+          minWidth: /.+-ff-.+/.test(id) ? "12rem" : "initial",
+          ...inputStyle,
+        }}
+        {...props}
+      >
+        {customOptions.map((opt, i) => (
           <option key={opt} value={i} style={inputStyle}>
             {opt}
           </option>
